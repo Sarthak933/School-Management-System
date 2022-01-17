ea22f0663ee4470eedffee4ec18efab97914e060
Sarthak Singh <96037391+Sarthak933@users.noreply.github.com>
2022-01-17 15:19:34 +0530

    Add files via upload

---
+++ about/about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About us </title>
+	<link rel="shortcut icon" type="image/png" href="favicon.png">
+	<link rel="stylesheet" type="text/css" href="style3.css">
+</head>
+<body>
+<center>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.1583154877612!2d73.90321881448101!3d25.028700844659014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39686c43edc8f0e3%3A0x7f3b932d14b57038!2sLakshmipat+Singhania+School!5e0!3m2!1sen!2sin!4v1548697236091" width="95%" height="500" frameborder="0" style="border:0" allowfullscreen></iframe></center>
+</div>
+
+<div class="about">
+<center>
+	<div class="media">
+		<?php
+			include "logo2.html"
+		?>
+		
+	</div>
+</center>
+</div>
+<div class="about1">
+	<div class="link">
+		<br>
+		<center><a href="link.php">Home</a> || <a href="about.php">About Us</a> || <a href="index.php">Portfolio</a> || <a href="contact.php">Contact</a></center>
+	</div>
+
+	<div class="copyrights">
+		<center>
+			All copyrights &copy reserved by LPSS
+		</center>
+	</div>
+
+	<?php
+		include "footer.php";
+	?>
+<br>
+
+
+</body>
+</html>
